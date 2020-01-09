86a654d226bf6fcb0c6eb4249cd41d49cacc0421
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2020-01-09 08:56:17 +0800

    scene

---
+++ TUMT_VR_HUI/Assets/Scenes/ani.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ani : MonoBehaviour {
+
+    float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(0, speed, 0,Space.Self);
+
+	}
+}
---
+++ TUMT_VR_HUI/Assets/Scenes/jum.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jum : MonoBehaviour {
+
+    public float Jum = 0.5f;
+
+    public int CC =15;
+
+    int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (count == 0)
+        {
+        this.transform.Translate(0, Jum, 0,Space.Self);
+            count++;
+           
+        }
+        else if (count == 10)
+        {
+            this.transform.Translate(0, -Jum , 0, Space.Self);
+            count++;
+
+        }
+        else if (count< CC && count >0)
+        {
+            
+            count ++;
+
+        }
+        else if (count == CC)
+        {
+
+            count = 0;
+
+        }
+
+    }
+}
