ac26208962994dd3b89da4a32a2e937583ecdc33
huihuiboy <55218458+huihuiboy@users.noreply.github.com>
2020-01-09 14:07:57 +0800

    game

---
+++ TUMT_VR_HUI/Assets/C#/game.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class game : MonoBehaviour {
+
+    public int score = 0;
+    public Text scr;
+
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "ball")
+        {
+            score += 10;
+            scr.text = "Score: " + score;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "ball")
+        {
+            score -= 10;
+            scr.text = "Score: " + score;
+        }
+    }
+}
